46324f999e86aa0db3bb455f657b6a25ccf49693
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-04-12 16:31:16 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -140,6 +140,15 @@ custom_packages() {
 
     # Download other luci-app-xxx
     # ......
+    # Download openclash
+    openclash_api="https://github.com/vernesong/OpenClash/blob/2aa811469c11f1985072574c653918f7062b189d/dev/luci-app-openclash_0.46.006-beta_all.ipk"
+    #
+    openclash_file="luci-app-openclash"
+    openclash_file_down="$(curl -s ${openclash_api} | grep "browser_download_url" | grep -oE "https.*${openclash_name}.*.ipk" | head -n 1)"
+    curl -fsSOJL ${openclash_file_down}
+    [[ "${?}" -eq "0" ]] || error_msg "[ ${openclash_file} ] download failed!"
+    echo -e "${INFO} The [ ${openclash_file} ] is downloaded successfully."
+    #
 
     sync && sleep 3
     echo -e "${INFO} [ packages ] directory status: $(ls -al 2>/dev/null)"
