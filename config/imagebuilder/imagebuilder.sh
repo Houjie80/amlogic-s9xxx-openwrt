0e5143f8fa463b7d7b2ebe7ba2532d813621de15
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-04-14 15:11:37 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -199,9 +199,9 @@ rebuild_firmware() {
         luci luci-base luci-compat luci-i18n-base-zh-cn luci-lib-base \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
         luci-mod-status luci-mod-system luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 \
-        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay dnsmasq-full -dnsmasq\
+        luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay dnsmasq-full -dnsmasq \
         vnstat2 vnstati2 openssh-sftp-server -procd-ujail adb usbutils -kmod-usb-net-asix-ax88179 -kmod-usb-net-asix \
-        kmod-usb3 luci-app-netspeedtest \
+        kmod-usb3 htop screen lsusb \
         \
         luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
